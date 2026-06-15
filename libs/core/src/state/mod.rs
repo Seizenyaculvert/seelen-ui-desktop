ff074fef86dedd94b0202eff2adfcb8c52d84caa
Seizenyaculvert <umutyazici62281@gmail.com>
2026-06-15 12:10:44 -0400

    Create libs/core/src/state/mod.rs

---
+++ libs/core/src/state/mod.rs
@@ -0,0 +1,29 @@
+mod icon_pack;
+mod placeholder;
+mod plugin;
+pub mod settings;
+mod theme;
+mod wallpaper;
+mod weg_items;
+mod widget;
+mod wm_layout;
+mod workspaces;
+
+pub use icon_pack::*;
+pub use placeholder::*;
+pub use plugin::*;
+pub use settings::*;
+pub use theme::*;
+pub use wallpaper::*;
+pub use weg_items::*;
+pub use widget::dialog::*;
+pub use widget::*;
+pub use wm_layout::*;
+pub use workspaces::*;
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SettingsBackup {
+    pub data: serde_json::Value,
+    pub updated_at: chrono::DateTime<chrono::Utc>,
+}
